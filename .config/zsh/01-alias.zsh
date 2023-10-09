f13f0569929ce45337bcb7682cc228826acdc2eb
yasunori <yasunori@gotoh.me>
2023-10-09 07:29:42 +0000

    sheldon

---
+++ .config/zsh/01-alias.zsh
@@ -0,0 +1,22 @@
+# エイリアス
+
+alias la='ls -a'
+alias ll='ls -l'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias mkdir='mkdir -p'
+
+# sudo の後のコマンドでエイリアスを有効にする
+alias sudo='sudo '
+
+# グローバルエイリアス
+alias -g L='| less'
+alias -g G='| grep'
+
+# ccatの色
+alias ccat='ccat -G String="_brown_" -G Plaintext="lightgrey" -G Punctuation="darkteal" -G Decimal="darkgreen" -G Keyword="green" -G Comment="darkgray" -G Tag="faint"'
+
+
